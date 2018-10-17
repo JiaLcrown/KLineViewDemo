1befe427891f392cce14e29b813b090537ff4576
Jia <jiafujia@lcrown.com>
2018-10-18 08:58:11 +0900

    origin

---
+++ KLineViewDemo/JFJView.swift
@@ -0,0 +1,13 @@
+//
+//  JFJView.swift
+//  KLineViewDemo
+//
+//  Created by jia on 2018/10/17.
+//  Copyright © 2018 jiafujia. All rights reserved.
+//
+
+import UIKit
+
+class JFJView: UIView {
+
+}
---
+++ KLineViewDemo/KLineViewDemo.h
@@ -0,0 +1,19 @@
+//
+//  KLineViewDemo.h
+//  KLineViewDemo
+//
+//  Created by jia on 2018/10/17.
+//  Copyright © 2018 jiafujia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KLineViewDemo.
+FOUNDATION_EXPORT double KLineViewDemoVersionNumber;
+
+//! Project version string for KLineViewDemo.
+FOUNDATION_EXPORT const unsigned char KLineViewDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KLineViewDemo/PublicHeader.h>
+
+
